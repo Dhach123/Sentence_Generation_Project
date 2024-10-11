50217009bbce01608cfeabc94d6cdc5d779e6e9f
Dharam chopda <chopdadharam143@gmail.com>
2024-10-11 18:14:33 +0530

    data validation added

--- src/textgeneration/config/configuration.py
+++ src/textgeneration/config/configuration.py
@@ -1,6 +1,6 @@
 from textgeneration.constants import *
 from textgeneration.utils.common import read_yaml, create_directories
-from textgeneration.entity import (DataIngestionConfig)
+from textgeneration.entity import (DataIngestionConfig,DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -29,5 +29,20 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
     
 
